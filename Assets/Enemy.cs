0abc6f2bab90033e67fc1d90a3e2527bba1b0aea
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2024-07-13 22:12:42 -0300

    die on player jump above

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -9,36 +8,44 @@ public class Enemy : MonoBehaviour
     private bool isColliding;
     public float hitDelay = 1f;
     public GameObject player = null;
+
     void Update()
     {
-      if (player != null && canDamage)
-      {
-         var hp = player.gameObject.GetComponent<Healthpoints>();
+        if (player != null && canDamage)
+        {
+            var hp = player.gameObject.GetComponent<Healthpoints>();
             if (!player.gameObject.GetComponent<Dash>().invincible)
             {
                 hp.TakeDamage(damage);
                 canDamage = false;
                 StartCoroutine(resetCanDamage(hitDelay));
             }
-      }
+        }
     }
-  void OnCollisionEnter2D(Collision2D other)
-  {
-        if (other.gameObject.CompareTag("Player") )
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
             player = other.gameObject;
+            if (player.GetComponent<Rigidbody2D>().velocity.y <= 0 && player.transform.position.y > transform.position.y)
+            {
+                Destroy(gameObject);
+            }
         }
-  }
+    }
+
     void OnCollisionExit2D(Collision2D other)
-  {
+    {
         if (other.gameObject.CompareTag("Player") && !canDamage)
         {
             player = null;
         }
-  }
-  IEnumerator resetCanDamage(float hitDelay)
-  {
-    yield return new WaitForSeconds(hitDelay);
-    canDamage = true;
-  }
+    }
+
+    IEnumerator resetCanDamage(float hitDelay)
+    {
+        yield return new WaitForSeconds(hitDelay);
+        canDamage = true;
+    }
 }
